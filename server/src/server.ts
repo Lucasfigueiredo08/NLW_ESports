b8b84cc33def65b54b8a11c2c3a5a1c9614cc95d
Lucas Figueiredo <lukas.liberato14@gmail.com>
2022-10-29 11:42:48 -0300

    Finalizando backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,16 @@
 import express from 'express'
 import {PrismaClient} from '@prisma/client'
-import { decompressFromBase64 } from '@prisma/client/runtime';
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
+import { convertMinutesToHourString } from './utils/convert-to-minutes-to-hourString';
+import cors from 'cors';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+
 /**
  * Log da query executada pelo prisma
  */
@@ -33,11 +41,27 @@ app.get("/games", async (request, response) => {
     return response.json(games)
 });
 
-app.post("/ads", (request, response) => {
-    return response.status(201).json([]);
+app.post("/games/:id/ads", async (request, response) => {
+    const gameId = request.params.id;
+    const body:any = request.body;
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(","),
+            hourStart: convertHourStringToMinutes(body.hourStart),
+            hourEnd: convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel
+        }
+    })
+
+    return response.status(201).json(ad);
 });
 
-""
+//TODO Método não está trazendo os anuncions por game 
 app.get("/games/:id/ads",async (request, response) => {
     // return response.send("Acessou ads");
     const gameId = request.params.id;
@@ -64,10 +88,12 @@ app.get("/games/:id/ads",async (request, response) => {
         ads.map( ad => {
             return {
                 ...ad,
-                weekDays: ad.weekDays.split(',')
+                weekDays: ad.weekDays.split(','),
+                hourStart: convertMinutesToHourString(ad.hourStart),
+                hourEnd: convertMinutesToHourString(ad.hourEnd)
             }
         })
-        );
+    );
 });
 
 
